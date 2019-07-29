506235f9e58ad9e0e38eff48fe5b52610596be15
Myles Kilgallon <github@myles.uk>
2019-07-29 12:57:21 +0800

    Love being back in Ruby

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,10 +3,12 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    if params["question"] = "I am going to work"
+    if params["question"].include? "?"
+      @answer = "Silly question, get dressed and go to work!"
+    elsif params["question"] == "I am going to work"
       @answer = "Great!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
     end
-
-
   end
 end
